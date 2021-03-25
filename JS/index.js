c30c52fdd47087a4c67c1cc0aa83efd4ff66ae19
Mindru-Bogdan <77194184+Mindru-Bogdan@users.noreply.github.com>
2021-03-25 10:32:29 +0200

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,124 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+   document.getElementById("myNav").style.width = "10%";
+}
+ 
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+   document.getElementById("myNav").style.width = "0%";
+}
+
+/*About Trailers*/
+/*Trailer 1*/
+function OpenCloseTrailer1() {
+   if(document.getElementById('Trailer1') .style.display=='none') {
+      document.getElementById('Trailer1') .style.display=''
+   }
+   else {
+      document.getElementById('Trailer1') .style.display='none'
+   }
+}
+
+/*Trailer 2*/
+function OpenCloseTrailer2() {
+   if(document.getElementById('Trailer2') .style.display=='none') {
+      document.getElementById('Trailer2') .style.display=''
+   }
+   else {
+      document.getElementById('Trailer2') .style.display='none'
+   }
+}
+
+/*Trailer 3*/
+function OpenCloseTrailer3() {
+   if(document.getElementById('Trailer3') .style.display=='none') {
+      document.getElementById('Trailer3') .style.display=''
+   }
+   else {
+      document.getElementById('Trailer3') .style.display='none'
+   }
+}
+
+/*Trailer 4*/
+function OpenCloseTrailer4() {
+   if(document.getElementById('Trailer4') .style.display=='none') {
+      document.getElementById('Trailer4') .style.display=''
+   }
+   else {
+      document.getElementById('Trailer4') .style.display='none'
+   }
+}
+
+/*Trailer 5*/
+function OpenCloseTrailer5() {
+   if(document.getElementById('Trailer5') .style.display=='none') {
+      document.getElementById('Trailer5') .style.display=''
+   }
+   else {
+      document.getElementById('Trailer5') .style.display='none'
+   }
+}
+
+/*Trailer 6*/
+function OpenCloseTrailer6() {
+   if(document.getElementById('Trailer6') .style.display=='none') {
+      document.getElementById('Trailer6') .style.display=''
+   }
+   else {
+      document.getElementById('Trailer6') .style.display='none'
+   }
+}
+
+/*Trailer 7*/
+function OpenCloseTrailer7() {
+   if(document.getElementById('Trailer7') .style.display=='none') {
+      document.getElementById('Trailer7') .style.display=''
+   }
+   else {
+      document.getElementById('Trailer7') .style.display='none'
+   }
+}
+
+/*Trailer 8*/
+function OpenCloseTrailer8() {
+   if(document.getElementById('Trailer8') .style.display=='none') {
+      document.getElementById('Trailer8') .style.display=''
+   }
+   else {
+      document.getElementById('Trailer8') .style.display='none'
+   }
+}
+
+/*Trailer 9*/
+function OpenCloseTrailer9() {
+   if(document.getElementById('Trailer9') .style.display=='none') {
+      document.getElementById('Trailer9') .style.display=''
+   }
+   else {
+      document.getElementById('Trailer9') .style.display='none'
+   }
+}
+
+/*CV buttons*/
+function OpenCloseIframeRo() {
+   if(document.getElementById('CV-Ro') .style.display=='none') {
+      document.getElementById('CV-Ro') .style.display=''
+   }
+   else {
+      document.getElementById('CV-Ro') .style.display='none'
+   }
+}
+
+function OpenCloseIframeEng() {
+   if(document.getElementById('CV-Eng') .style.display=='none') {
+      document.getElementById('CV-Eng') .style.display=''
+   }
+   else {
+      document.getElementById('CV-Eng') .style.display='none'
+   }
+}
+
+
+
+
+
